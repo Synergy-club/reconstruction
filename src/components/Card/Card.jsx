2a3e3fa7dd144de1d9c7af9aafab50fa3e064cb1
Daniele Mazzola <mazzoladaniele@gmail.com>
2024-12-10 13:13:47 +0100

    UPDATE height nav, CREATE Footer component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,21 +8,23 @@ const Card = ({ array }) => {
         Object.keys(array).length > 0 &&
         Object.values(array).map((element, index) => (
           <div key={index} className='card__item'>
-            <div className='card__content-img'>
-              <img src={element.image} alt={element.title} />
-            </div>
-            <div className='card__content'>
-              <div>
-                <h2>{element.title}</h2>
-                <article>
-                  <p>{element.description}</p>
-                  <p>{element.description_}</p>
-                </article>
+            <div >
+              <div className='card__content-img'>
+                <img src={element.image} alt={element.title} />
+              </div>
+              <div className='card__content show'>
+                <div>
+                  <h2>{element.title}</h2>
+                  <article>
+                    <p>{element.description}</p>
+                    <p>{element.description_}</p>
+                  </article>
+                </div>
               </div>
             </div>
-            <div className='card__content show'>
-              {element.span && <span>*{element.span}</span>}
-              <button className='button'>
+            <div className='card__content'>
+              {element.span && <span className='show'>{element.span}</span>}
+              <button className='button card__content-links show'>
                 <a
                   href={element.link}
                   rel='noopener noreferrer'
---
+++ src/components/Footer/utils/footer.js
@@ -0,0 +1,25 @@
+export const footer = [
+  {
+    title: 'Contacto',
+    items: [
+      { name: 'gmail', link: '#' },
+      { name: 'whatsapp', link: '#' },
+      { name: 'instagram', link: '#' }
+    ]
+  },
+  {
+    title: 'Nuestras redes sociales',
+    items: [
+      { name: 'youtube', link: '#' },
+      { name: 'instagram', link: '#' },
+      { name: 'linkedin', link: '#' }
+    ]
+  },
+  {
+    title: 'Colaboradores',
+    items: [
+      { name: 'option 1', link: '#' },
+      { name: 'option 2', link: '#' }
+    ]
+  }
+];
